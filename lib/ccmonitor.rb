e68ca7a3bb14ecfa9755205f00e9269bc8e7074c
Juan Ibiapina <juanibiapina@thoughtworks.com>
2011-04-16 23:37:53 -0300

    added helper method for reading configurations

--- lib/ccmonitor.rb
+++ lib/ccmonitor.rb
@@ -7,8 +7,14 @@ set :CONFIG, YAML.load(File.read("config/config.yml"))
 set :root,  File.join(File.dirname(__FILE__), "..")
 set :static, true
 
+helpers do
+  def config
+    settings.CONFIG
+  end
+end
+
 get "/" do
-  xml_file = open(settings.CONFIG[:FEED_URL])
+  xml_file = open(config[:FEED_URL])
 end
 
 get "/wall" do # this is just a dummy/stub
