f854ddbb1f39fe1a48c57fe824cc743016263968
Juan Ibiapina <juanibiapina@thoughtworks.com>
2011-04-19 17:54:54 -0300

    refactored projects parser test

--- spec/projects_parser_spec.rb
+++ spec/projects_parser_spec.rb
@@ -6,32 +6,44 @@ describe ProjectsParser do
   context 'when created with an xml' do
 
     before do
-      @xml = "<Projects><Project name='Awesome' lastBuildStatus='Success' /></Projects>"
+      @xml = %!
+      <Projects>
+        <Project name='a cool name' lastBuildStatus='Success' />
+        <Project name='an even better name' lastBuildStatus='Failure' />
+      </Projects>
+      !
+
+      @project1 = Hash.new
+      @project1['name'] = 'a cool name'
+      @project1['state'] = 'success'
+
+      @project2 = Hash.new
+      @project2['name'] = 'an even better name'
+      @project2['state'] = 'failure'
+
+      @expected = Hash.new
+      @expected['projects'] = []
+
       @parser = ProjectsParser.new
     end
 
     it 'should generate a tag for each project' do
-      expected = Hash.new
-      a_project = Hash.new
-      a_project['name'] = 'Awesome'
-      a_project['state'] = 'success'
-      expected['projects'] = []
-      expected['projects'] << a_project
+      @expected['projects'] << @project1
+      @expected['projects'] << @project2
 
       result = @parser.parse @xml
 
-      result.should == expected
+      result.should == @expected
     end
 
     it 'should filter by project name' do
-      expected = Hash.new
-      expected['projects'] = []
+      @expected['projects'] << @project2
 
-      @parser.add_filter('Awesome')
+      @parser.add_filter('cool')
 
       result = @parser.parse @xml
 
-      result.should == expected
+      result.should == @expected
     end
 
   end
