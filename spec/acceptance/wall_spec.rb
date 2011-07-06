c50347ba15a60ecb12728140c73a1f82bb05779b
Juan Ibiapina <juanibiapina@thoughtworks.com>
2011-07-06 20:01:19 -0300

    Adding a hidden box with the sounds.

--- spec/acceptance/wall_spec.rb
+++ spec/acceptance/wall_spec.rb
@@ -10,4 +10,16 @@ describe 'wall' do
   it 'should have a failed build' do
     find('#failure_box').find('h5').text.should == 'at.e2e2.us.functional.browse.regression'
   end
+
+  it 'should have a box for the sounds' do
+    page.should have_xpath "//div[@id='sounds']"
+  end
+
+  it 'should have a success sound' do
+    page.should have_xpath "//audio[@id='success']"
+  end
+
+  it 'should have a failure sound' do
+    page.should have_xpath "//audio[@id='failure']"
+  end
 end
