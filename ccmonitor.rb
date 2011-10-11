3a0601e645e9a7a5dde27d5b25a3f961dfc3ba21
juanibiapina <juanibiapina@gmail.com>
2011-10-11 16:21:43 -0300

    Handling feed reader failures.

--- ccmonitor.rb
+++ ccmonitor.rb
@@ -9,19 +9,23 @@ Dir["lib/**/*.rb"].each { |lib_file| load lib_file }
 
 config_options = YAML.load(File.read("config/config.#{settings.environment}.yml"))
 
+def log(msg)
+  puts "#{Time.now}: #{msg}"
+end
+
 unless settings.environment == :test
   feed_reader = FeedReader.new config_options[:FEED_URL]
   Thread.new do 
     while true
+      log 'Reading feed.'
+
       begin
-        Timeout::timeout(30) do
-          puts 'Reading feed.'
-          feed_reader.run
-          puts 'Feed updated.'
-        end
-      rescue Timeout::Error => e
-        puts 'Timeout reading feed.'
+        feed_reader.run
+      rescue => e
+        log "Exception thrown while reading feed: #{e}"
       end
+
+      log 'Feed updated. Sleeping for 60 seconds.'
       sleep(60)
     end
   end
