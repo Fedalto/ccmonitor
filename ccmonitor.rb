fda4633161532d82ee5bee6787af8a3410298e15
juanibiapina <juanibiapina@gmail.com>
2011-10-13 17:41:50 -0300

    Using only one thread to read feeds.

--- ccmonitor.rb
+++ ccmonitor.rb
@@ -10,13 +10,16 @@ Dir["lib/**/*.rb"].each { |lib_file| load lib_file }
 config_options = YAML.load(File.read("config/config.#{settings.environment}.yml"))
 
 unless settings.environment == :test
+  feeds = []
   config_options[:FEEDS].each do |feed_url|
-    feed_reader = FeedReader.new feed_url
-    Thread.new do 
-      while true
-        feed_reader.run
-        sleep(60)
+    feeds << FeedReader.new(feed_url)
+  end
+  Thread.new do 
+    while true
+      feeds.each do |feed|
+        feed.run
       end
+      sleep(60)
     end
   end
 end
