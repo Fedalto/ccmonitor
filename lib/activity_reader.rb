ec091933f5302ba7e51cf40536fedaf54ef6aac3
juanibiapina <juanibiapina@gmail.com>
2011-10-15 03:49:45 -0300

    Fixing problem with non application builds.

--- lib/activity_reader.rb
+++ lib/activity_reader.rb
@@ -18,15 +18,17 @@ class ActivityReader
 
     begin
       Nokogiri::XML(open(@url)).xpath('//Project').each do |project|
-        version = project.get_attribute('name').split("-")[0]
-        cdr = project.get_attribute('name').split("-")[1..-1].join('.')
-        name = cdr.split('.')[0]
-        type = cdr.split('.')[1..-1].join('.')
+        unless project.get_attribute('name').count('-') < 2
+          version = project.get_attribute('name').split("-")[0]
+          cdr = project.get_attribute('name').split("-")[1..-1].join('.')
+          name = cdr.split('.')[0]
+          type = cdr.split('.')[1..-1].join('.')
 
-        build_info = BuildInfo.find_by_id(version + '-' + name + '-' + type)
-        unless build_info.nil?
-          build_info.activity = project.get_attribute('activity').downcase
-          build_info.save!
+          build_info = BuildInfo.find_by_id(version + '-' + name + '-' + type)
+          unless build_info.nil?
+            build_info.activity = project.get_attribute('activity').downcase
+            build_info.save!
+          end
         end
       end
     rescue => e
