4de980fd472253dba9c0c3769ded25e64e16bafa
juanibiapina <juanibiapina@gmail.com>
2011-10-14 22:22:42 -0300

    Separating some acceptance tests.

--- spec/acceptance/wall_spec.rb
+++ spec/acceptance/wall_spec.rb
@@ -39,42 +39,6 @@ describe 'wall' do
   end
 end
 
-describe 'name filter' do
-  it 'should include only builds called otherproject' do
-    visit '/wall?include_names=otherproject' 
-
-    all('.cell').each do |cell|
-      cell.find('h4').text.should == 'OTHERPROJECT'
-    end
-  end
-
-  it 'should not include builds called otherproject' do
-    visit '/wall?exclude_names=otherproject' 
-
-    all('.cell').each do |cell|
-      cell.find('h4').text.should == 'PROJECT'
-    end
-  end
-end
-
-describe 'type filter' do
-  it 'should include only builds with type quick' do
-    visit '/wall?include_types=quick' 
-
-    all('.cell').each do |cell|
-      cell.find('h5').text.should == 'quick'
-    end
-  end
-
-  it 'should not include builds with type quick' do
-    visit '/wall?exclude_names=otherproject' 
-
-    all('.cell').each do |cell|
-      cell.find('h5').text.should == 'package'
-    end
-  end
-end
-
 describe 'feed unavailable' do
   before(:each) do
     FeedReader.new('invalid_file.xml').run
