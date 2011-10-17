d72de5e5b01397cf6b14ea87e71d9a502a00473c
juanibiapina <juanibiapina@gmail.com>
2011-10-17 18:33:31 -0200

    Restored rotten builds color.

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -97,6 +97,13 @@ dashboard.cell = function (specs) {
 
     activity === 'building' && element.addClass("building");
 
+    var max_time = 7 * 24 * 60 * 60;
+
+    var ra = Math.floor(time_since_green / (max_time / 160));
+    var ga = Math.floor(time_since_green / (max_time / 40));
+    var ba = Math.floor(time_since_green / (max_time / 40));
+    state === 'failure' && recent === false && element.css('background-color', 'rgb(' + (160-ra) + ',' + (40-ga) + ',' + (40-ba) + ')');
+
     return element;
   };
 
