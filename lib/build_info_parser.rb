36b49db6b9f3c508e7ab5455323eea111a5d8420
juanibiapina <juanibiapina@gmail.com>
2011-12-05 19:21:59 -0200

    Handle project with empty name.

--- lib/build_info_parser.rb
+++ lib/build_info_parser.rb
@@ -4,25 +4,27 @@ class BuildInfoParser
   def self.parse(xml)
     @data = []
     Nokogiri::XML(xml).xpath('//Project').each do |project|
-      version = project.get_attribute('name').split("-")[0]
-      cdr = project.get_attribute('name').split("-")[1..-1].join('.')
-      name = cdr.split('.')[0]
-      type = cdr.split('.')[1..-1].join('.')
-      assignedTo = project.get_attribute('assignedTo')
+      if not project.get_attribute('name').strip().empty?
+        version = project.get_attribute('name').split("-")[0]
+        cdr = project.get_attribute('name').split("-")[1..-1].join('.')
+        name = cdr.split('.')[0]
+        type = cdr.split('.')[1..-1].join('.')
+        assignedTo = project.get_attribute('assignedTo')
 
-      build_info = BuildInfo.new
-      build_info.name = name
-      build_info.state = project.get_attribute('lastBuildStatus').downcase
-      build_info.activity = project.get_attribute('activity').downcase
-      build_info.version = version
-      build_info.build_type = type
-      build_info.buildUrl = project.get_attribute('webUrl')
-      build_info.assignedTo = assignedTo.empty? ? "-------" : assignedTo
-      build_info.assignUrl = project.get_attribute('assignUrl')
+        build_info = BuildInfo.new
+        build_info.name = name
+        build_info.state = project.get_attribute('lastBuildStatus').downcase
+        build_info.activity = project.get_attribute('activity').downcase
+        build_info.version = version
+        build_info.build_type = type
+        build_info.buildUrl = project.get_attribute('webUrl')
+        build_info.assignedTo = assignedTo.empty? ? "-------" : assignedTo
+        build_info.assignUrl = project.get_attribute('assignUrl')
 
-      build_info.id = version + '-' + name + '-' + type
+        build_info.id = version + '-' + name + '-' + type
 
-      @data << build_info
+        @data << build_info
+      end
     end
     @data
   end
--- spec/build_info_parser_spec.rb
+++ spec/build_info_parser_spec.rb
@@ -14,6 +14,13 @@ describe BuildInfoParser do
       </Projects>
       !
 
+      @xml_with_empty_project = %!
+      <Projects>
+        <Project name='trunk-acoolname-quick' activity='Sleeping' lastBuildStatus='Success' webUrl='someUrl' assignedTo="" assignUrl="assignUrl"/>
+        <Project name='' activity='Sleeping' lastBuildStatus='Success' webUrl='someUrl' assignedTo="person" assignUrl="assignUrl"/>
+      </Projects>
+      !
+
       @project1 = BuildInfo.new({'id' => 'trunk-acoolname-quick', 'name' => 'acoolname', 'version' => 'trunk', 'state' => 'success', 'activity' => 'sleeping', 'build_type' => 'quick', 'buildUrl' => 'someUrl', 'assignedTo' => '-------', 'assignUrl' => 'assignUrl'})
       @project2 = BuildInfo.new({'id' => 'trunk-abettername-package', 'name' => 'abettername', 'version' => 'trunk', 'state' => 'success','activity' => 'sleeping',  'build_type' => 'package', 'buildUrl' => 'someUrl', 'assignedTo' => 'person', 'assignUrl' => 'assignUrl'})
       @project3 = BuildInfo.new({'id' => '1.0-anevenbettername-isolated.test', 'name' => 'anevenbettername', 'version' => '1.0', 'state' => 'failure', 'activity' => 'sleeping', 'build_type' => 'isolated.test', 'buildUrl' => 'someUrl', 'assignedTo' => 'person', 'assignUrl' => 'assignUrl'})
@@ -22,6 +29,10 @@ describe BuildInfoParser do
 
     it 'should generate a tag for each project' do
       BuildInfoParser.parse(@xml).should == [ @project1, @project2, @project3 ]
+    end
+
+    it 'should not fail when projects have empty names' do
+      BuildInfoParser.parse(@xml_with_empty_project).should == [ @project1 ]
     end
 
   end
