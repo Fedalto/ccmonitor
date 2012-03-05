e38fd4ffd98b3a09972d8ec85f2fd8463e5cf836
Duda Dornelles <ddornell@thoughtworks.com>
2012-03-05 11:26:26 -0300

    fixing parse of component test page

--- lib/component_test_reader.rb
+++ lib/component_test_reader.rb
@@ -2,12 +2,13 @@ require 'net/http'
 
 class ComponentTestReader
   attr_reader :test_status
+
   def initialize(url)
    @url = url
   end 
  
   def parse_page
-    @test_status = get_html(@url).grep(/component tests failed/) ? :failure : :success
+    @test_status = get_html(@url) =~ /component tests failed/ ? :failure : :success
   end
 
   private
--- spec/component_test_reader_spec.rb
+++ spec/component_test_reader_spec.rb
@@ -3,6 +3,8 @@ require 'component_test_reader'
 describe ComponentTestReader do
   before :each do 
     @html_failing_component_tests = "<html><pre class=\"compile-error-data\">PMD Identified 4 unit test violations in SolrPerfUtils's unit tests.Tests FAILED<br class=\"none\"/>One ore more component tests failed.  Current module is SolrSearchConfiguration<br class=\"none\"/></html>"
+    @html_success_component_tests = ""
+
     @cruise_url = "some_url.com"
     @reader = ComponentTestReader.new(@cruise_url)
   end
@@ -12,6 +14,12 @@ describe ComponentTestReader do
       @reader.should_receive(:get_html).with(@cruise_url).and_return(@html_failing_component_tests)
       @reader.parse_page
       @reader.test_status.should == :failure 
+    end
+
+    it "succeds if the component tests are passing" do
+      @reader.should_receive(:get_html).with(@cruise_url).and_return(@html_success_component_tests)
+      @reader.parse_page
+      @reader.test_status.should == :success 
     end
   end 
 
